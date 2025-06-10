e1e4c418ef30f39e08fd028fa9a585f3f35ea303
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-06-10 12:28:45 -0300

    feat: str(string) types

---
+++ aula3.py
@@ -0,0 +1,21 @@
+"""
+python = Liguagem de Programação 
+Tipo de tipagem = Dinamica / Forte
+str -> string -> texto
+Strings são texto dentro de aspas simples ou duplas
+"""
+
+print(123)
+
+
+# Aspas simples
+print('Um texto fica dentro de aspas')
+
+# Aspas duplas
+print("Um texto fica dentro de aspas")
+
+# Escape
+print("Fellype \"Moura\"")
+
+# r
+print(r"Fellype \"Moura\"")
