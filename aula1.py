d8a1bc9fdfed79ec3a321943946371f673095b79
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-06-10 11:58:03 -0300

    feat: show on screen, comment and line break

--- aula1.py
+++ aula1.py
@@ -1,2 +1,7 @@
+print('outra coisa')  # comentario em um unica linha
+""""
+DocString
+para escrever notas em
+linhas
+"""
 print('Hello World!')
-print('outa coisa')
---
+++ aula2.py
@@ -0,0 +1,3 @@
+print(12, 34, 56, sep="-")  
+print(123, 123, 123, sep='---', end='\n quebrou a linha ')  
+print(10987654321, sep='')  
