b6db38eda2243fbf52bd043d5d20fccf0429fcdb
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-06-11 13:24:46 -0300

    feat: programming exercise for calculation

---
+++ aula12.py
@@ -0,0 +1,12 @@
+nome = 'Fellype Moura'
+altura = 1.64
+peso = 68
+imc = peso / altura ** 2
+
+print(nome, 'tem', altura, 'de altura,',)
+print('pesa', peso, 'quilos e seu imc é',)
+print(imc)
+
+# fellype tem 1.64 de altura,
+# pesa 68 quilos e seu IMC é
+# 25.282569898869724
