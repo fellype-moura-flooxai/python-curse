9d16ecea9d31d1c1b16c3cd5f5302c9d27dd3d5d
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-06-12 14:09:50 -0300

    feat: example of condition with while

---
+++ aula34.py
@@ -0,0 +1,12 @@
+"""
+Repetições
+while (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+Loop infinito -> Quando um código não tem fim
+"""
+
+numero1 = 0
+
+while numero1 <= 10 :
+    print(numero1)
+    numero1 += 1
