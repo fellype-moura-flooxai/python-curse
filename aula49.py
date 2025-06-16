d004266e29cc9a4a61f4c3c8d07ea2b4287a50b9
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-06-16 11:41:45 -0300

    feat: exercise displaying list indexes

---
+++ aula49.py
@@ -0,0 +1,15 @@
+
+"""
+Exercício
+Exiba os índices da lista
+0 Maria
+1 Helena
+2 Luiz
+"""
+lista = ['Maria', 'Helena', 'Luiz']
+itenslist = range(len(lista))
+
+print(itenslist)
+
+for loops in itenslist:
+    print(loops, lista[loops])
