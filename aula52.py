cda9635fd31c72f7b271a1f151445e29e11e7b5b
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-06-16 14:23:46 -0300

    feat: teacher's solution exercise

--- aula52.py
+++ aula52.py
@@ -5,6 +5,9 @@ inserir, apagar e listar valores da sua lista
 Não permita que o programa quebre com 
 erros de índices inexistentes na lista.
 """
+
+# meu código
+
 lista = ['maça', 'limão', 'pera', 'banana', 'uva']
 
 while True:
@@ -49,4 +52,42 @@ while True:
         break
 
 
+# do professor 
 
+
+# import os
+
+# lista = []
+
+# while True:
+#     print('Selecione uma opção')
+#     opcao = input('[i]nserir [a]pagar [l]istar: ')
+
+#     if opcao == 'i':
+#         os.system('clear')
+#         valor = input('Valor: ')
+#         lista.append(valor)
+#     elif opcao == 'a':
+#         indice_str = input(
+#             'Escolha o índice para apagar: '
+#         )
+
+#         try:
+#             indice = int(indice_str)
+#             del lista[indice]
+#         except ValueError:
+#             print('Por favor digite número int.')
+#         except IndexError:
+#             print('Índice não existe na lista')
+#         except Exception:
+#             print('Erro desconhecido')
+#     elif opcao == 'l':
+#         os.system('clear')
+
+#         if len(lista) == 0:
+#             print('Nada para listar')
+
+#         for i, valor in enumerate(lista):
+#             print(i, valor)
+#     else:
+#         print('Por favor, escolha i, a ou l.')
